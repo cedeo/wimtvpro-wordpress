cf80e507f5af471b6424f53a3a7f138930f371d6
Simona <ssimo84@gmail.com>
2013-10-31 16:34:30 +0100

    Fixed bug jwplayer android

--- pages/embeddedPlayList.php
+++ pages/embeddedPlayList.php
@@ -4,9 +4,11 @@
 	if (!isset($itShortcode))
 		include("../../../../wp-load.php");
 	
-	
+	$user_agent = $_SERVER['HTTP_USER_AGENT'];
   	$idPlayList=$_GET['id'];
 	$page=$_GET['page'];
+
+	//Read Playlist's Videos into DB
 	
     $table_name = $wpdb->prefix . 'wimtvpro_playlist';
   	$record = $wpdb->get_results("SELECT listVideo,name FROM {$table_name} WHERE id='" . $idPlayList . "'");
@@ -14,6 +16,7 @@
 	$listVideo = $record[0]->listVideo;
 	$title = $record[0]->name;
      if (!isset($page))	{
+		 
     		$height = get_option("wp_heightPreview") +190;
 			$width = get_option("wp_widthPreview") +280;
 			$widthP = get_option("wp_widthPreview") +250; 	
@@ -35,6 +38,7 @@
 	
 	}
 	
+	//Read Data videos
 	
 	$videoList = explode (",",$listVideo);
 	$sql_where  = " 1=2 ";
@@ -58,7 +62,7 @@
 		$videoArr[0] = $videoT;
 		$dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf";
 		
-		$configFile  = wimtvpro_viever_jwplayer($_SERVER['HTTP_USER_AGENT'],$videoT->contentidentifier,$videoArr,$dirJwPlayer);
+		$configFile  = wimtvpro_viever_jwplayer($user_agent,$videoT->contentidentifier,$videoArr,$dirJwPlayer);
 		if (!isset($videoT->urlThumbs)) $thumbs[1] = "";
 		else $thumbs = explode ('"',$videoT->urlThumbs);
 		
@@ -68,26 +72,54 @@
 
 	$uploads_info = wp_upload_dir();
 
+	//Create jwplayer playlist
+
 	$dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf"; 
 	$code .= "<script type='text/javascript'>jwplayer('container-" . $idPlayList . "').setup({";
-	//$code .=  "modes: [{type: 'flash',src:'" . $dirJwPlayer . "'}],";
-	$code .="'repeat':'always',";
-	$uploads_info = wp_upload_dir();
-	$nomeFilexml  = wimtvpro_searchFile($uploads_info["basedir"] .  "/skinWim/" . get_option('wp_nameSkin'),"xml");
-    if (get_option('wp_nameSkin')!="") {
-       	$directory =  $uploads_info["baseurl"] .  "/skinWim"; 
-        $skin = "'skin':'" . $directory  . "/" . get_option('wp_nameSkin') . "/" . $nomeFilexml . "',";
-    }
+	$urlPlay = explode("$$",$video[0]->urlPlay);
 	
-	$code .= $skin . $dimensions . "'flashplayer':'" .  $dirJwPlayer . "','playlist': [" .  $playlist . "],'playlist.position': 'right',	'playlist.size': '" . $playlistSize  . "'});</script>&nbsp;";
-			echo $code;
+	//Check if browser is mobile
+	$isiPad = (bool) strpos($user_agent,'iPad');
+    $isiPhone = (bool) strpos($user_agent,'iPhone');
+    $isAndroid = (bool) strpos($user_agent,'Android');
+	$mobile = false;
+	if ($isiPad  || $isiPhone || $isAndroid) {
+		$mobile=true;
+	}
+	
+	//Mobile: HTML5, Web: Flash
+    if (!$mobile) {
+		$code .=  "modes: [{type: 'flash',src:'" . $dirJwPlayer . "'}],";
+	} else
+		$code .=  "modes: [{type: 'html5'}],";
+		
+	$code .="'repeat':'always',";
+	$skin = "";
+
+		$uploads_info = wp_upload_dir();
+		$nomeFilexml  = wimtvpro_searchFile($uploads_info["basedir"] .  "/skinWim/" . get_option('wp_nameSkin'),"xml");
+		if (get_option('wp_nameSkin')!="") {
+			$directory =  $uploads_info["baseurl"] .  "/skinWim"; 
+			$skin = "'skin':'" . $directory  . "/" . get_option('wp_nameSkin') . "/" . $nomeFilexml . "',";
+		}
+	
+	$code .= $skin . $dimensions . "'fallback':'false',";
+	$code .= "playlist: [" . $playlist . "],";
+	$code .= "'playlist.position': 'right',	'playlist.size': '" . $playlistSize  . "'});</script>";
+	echo $code;
+	
+	//echo htmlspecialchars($code);
 	
 	if (!isset($itShortcode)){
 		
 		if (!isset($page))	{
-			echo "<p>Embedded:</p><textarea style='resize: none; width:90%;height:70px;font-size:10px' readonly='readonly' onclick='this.focus(); this.select();'>" . htmlentities($code) . "</textarea>";
-			echo "</div>";
+			echo "<div style='float:left; width:50%;'>
+			Embedded:<textarea style='resize: none; width:90%;height:70px;font-size:10px' readonly='readonly' onclick='this.focus(); this.select();'>" . htmlentities($code) . "</textarea></div>";
+			
+			echo "<div style='float:left; width:50%;'>Shortcode:<textarea style='resize: none; width:90%;height:70px;font-size:20px' readonly='readonly' onclick='this.focus(); this.select();'>[playlistWimtv id='" . $idPlayList . "']</textarea></div>";
+			
 		}
 	}
+	echo "</div>";
 
 ?>

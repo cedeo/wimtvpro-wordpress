813f992a709e7bf656f1e8ff0b3542a393b8e9bd
Walter Danilo Galante <walterdangalante@gmail.com>
2013-11-21 15:13:00 +0100

    Fixed playlist script

--- functions/jwPlayer.php
+++ functions/jwPlayer.php
@@ -23,6 +23,8 @@ function wimtvpro_viever_jwplayer($userAgent, $contentId,  $dirJwPlayer) {
     } else if ($isAndroid) {
         $configFile = "file: '" . $url . "',";
     } else {
+        $url_parts = explode("/", $url);
+        $url = $url_parts[count($url_parts) -1];
         $configFile = "'flashplayer':'" . $dirJwPlayer . "','file': '" . $url . "','streamer':'" . $streamer . "',";
     }
     return $configFile;

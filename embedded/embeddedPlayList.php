eadc9c00aba79ef1ee419257156ebacf8bb19fe5
Walter Danilo Galante <walterdangalante@gmail.com>
2013-11-21 14:14:28 +0100

    Fixed embedded playlist code

--- embedded/embeddedPlayList.php
+++ embedded/embeddedPlayList.php
@@ -41,71 +41,70 @@ for ($i=0;$i<count($videoList);$i++){
 			array_push($array_videos_new_drupal, $record_new);
 		}
 	}
-	
-	$playlist = "";
-	foreach ($array_videos_new_drupal as $videoT){
-		$videoArr[0] = $videoT;
-		$dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf";
-	
-		$configFile  = wimtvpro_viever_jwplayer($user_agent,$videoT->contentidentifier,$dirJwPlayer);
-		if (!isset($videoT->urlThumbs)) {
-			$thumbs[1] = "";
-		}
-		else {
-			$thumbs = explode ('"',$videoT->urlThumbs);
-		}
-		$playlist .= "{" . $configFile . " 'image':'" . $thumbs[1]  . "','title':'" . str_replace ("+"," ",urlencode($videoT->title)) . "'},";
-	
-	}
-	
-	$uploads_info = wp_upload_dir();
-	
-	//Create jwplayer playlist
-	
-	$dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf"; 
-	$code .= "<script type='text/javascript'>jwplayer('container-" . $idPlayList . "').setup({";
-	$urlPlay = explode("$$",$video[0]->urlPlay);
-	
-	//Check if browser is mobile
-	$isiPad = (bool) strpos($user_agent,'iPad');
-	$isiPhone = (bool) strpos($user_agent,'iPhone');
-	$isAndroid = (bool) strpos($user_agent,'Android');
-	$mobile = false;
-	if ($isiPad  || $isiPhone || $isAndroid) {
-		$mobile=true;
-	}
-	
-	//Mobile: HTML5, Web: Flash
-	if (!$mobile) {
-		$code .=  "modes: [{type: 'flash',src:'" . $dirJwPlayer . "'}],";
-	} else {
-		$code .=  "modes: [{type: 'html5'}],";
-	}
-	$code .="'repeat':'always',";
-	$skin = "";
-	
-	$uploads_info = wp_upload_dir();
-	$nomeFilexml  = wimtvpro_searchFile($uploads_info["basedir"] .  "/skinWim/" . get_option('wp_nameSkin'),"xml");
-	if (get_option('wp_nameSkin')!="") {
-		$directory =  $uploads_info["baseurl"] .  "/skinWim"; 
-		$skin = "'skin':'" . $directory  . "/" . get_option('wp_nameSkin') . "/" . $nomeFilexml . "',";
-	}
-	
-	$code .= $skin . $dimensions . "'fallback':'false',";
-	$code .= "playlist: [" . $playlist . "],";
-	$code .= "'playlist.position': 'right',	'playlist.size': '" . $playlistSize  . "'});</script>";
-	$output_playlist .= $code;
+}
+$playlist = "";
+foreach ($array_videos_new_drupal as $videoT){
+    $videoArr[0] = $videoT;
+    $dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf";
 
-	if ($is_admin){
-		$output_playlist .= "<div style='float:left; width:50%;'>
-	Embedded:<textarea style='resize: none; width:90%;height:70px;font-size:10px' readonly='readonly' onclick='this.focus(); this.select();'>" . htmlentities($code) . "</textarea></div>";
-	
-		$output_playlist .= "<div style='float:left; width:50%;'>Shortcode:<textarea style='resize: none; width:90%;height:70px;font-size:20px' readonly='readonly' onclick='this.focus(); this.select();'>[playlistWimtv id='" . $idPlayList . "']</textarea></div>";
-	}
-	
-	$output_playlist .= "</div>";
-	wp_reset_query();
-	echo $output_playlist;	
+    $configFile  = wimtvpro_viever_jwplayer($user_agent,$videoT->contentidentifier,$dirJwPlayer);
+    if (!isset($videoT->urlThumbs)) {
+        $thumbs[1] = "";
+    }
+    else {
+        $thumbs = explode ('"',$videoT->urlThumbs);
+    }
+    $playlist .= "{" . $configFile . " 'image':'" . $thumbs[1]  . "','title':'" . str_replace ("+"," ",urlencode($videoT->title)) . "'},";
 
 }
+
+$uploads_info = wp_upload_dir();
+
+//Create jwplayer playlist
+
+$dirJwPlayer = plugin_dir_url(dirname(__FILE__)) . "script/jwplayer/player.swf";
+$code .= "<script type='text/javascript'>jwplayer('container-" . $idPlayList . "').setup({";
+$urlPlay = explode("$$",$video[0]->urlPlay);
+
+//Check if browser is mobile
+$isiPad = (bool) strpos($user_agent,'iPad');
+$isiPhone = (bool) strpos($user_agent,'iPhone');
+$isAndroid = (bool) strpos($user_agent,'Android');
+$mobile = false;
+if ($isiPad  || $isiPhone || $isAndroid) {
+    $mobile=true;
+}
+
+//Mobile: HTML5, Web: Flash
+if (!$mobile) {
+    $code .=  "modes: [{type: 'flash',src:'" . $dirJwPlayer . "'}],";
+} else {
+    $code .=  "modes: [{type: 'html5'}],";
+}
+$code .="'repeat':'always',";
+$skin = "";
+
+$uploads_info = wp_upload_dir();
+$nomeFilexml  = wimtvpro_searchFile($uploads_info["basedir"] .  "/skinWim/" . get_option('wp_nameSkin'),"xml");
+if (get_option('wp_nameSkin')!="") {
+    $directory =  $uploads_info["baseurl"] .  "/skinWim";
+    $skin = "'skin':'" . $directory  . "/" . get_option('wp_nameSkin') . "/" . $nomeFilexml . "',";
+}
+
+$code .= $skin . $dimensions . "'fallback':'false',";
+$code .= "playlist: [" . $playlist . "],";
+$code .= "'playlist.position': 'right',	'playlist.size': '" . $playlistSize  . "'});</script>";
+$output_playlist .= $code;
+
+if ($is_admin){
+    $output_playlist .= "<div style='float:left; width:50%;'>
+Embedded:<textarea style='resize: none; width:90%;height:70px;font-size:10px' readonly='readonly' onclick='this.focus(); this.select();'>" . htmlentities($code) . "</textarea></div>";
+    $output_playlist .= "<div style='float:left; width:50%;'>Shortcode:<textarea style='resize: none; width:90%;height:70px;font-size:20px' readonly='readonly' onclick='this.focus(); this.select();'>[playlistWimtv id='" . $idPlayList . "']</textarea></div>";
+}
+
+$output_playlist .= "</div>";
+wp_reset_query();
+echo $output_playlist;
+
+
 ?>
